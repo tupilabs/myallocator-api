c61e7d562657c181e75a00b97a391a749d0a05c1
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-06-03 19:39:46 -0300

    Adding composer and base class

---
+++ src/Myallocator.php
@@ -0,0 +1,17 @@
+<?php
+
+class Myallocator {
+
+    public $userId = 0;
+
+    public $userPassword = "";
+
+    public $vendorId = 0;
+
+    public $vendorPassword = "";
+
+    public $root = 'http://api.myallocator.com/';
+
+    public $channels = array();
+
+}
